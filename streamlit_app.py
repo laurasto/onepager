6660717e252700165996f4b2810b8b8666452760
Laura Astola <laura.astola@accenture.com>
2026-06-17 10:20:05 +0200

    added certificates

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
-# An interactive format of our beloved OnePager
+# An GitHub "compatible" mini cv
 
 import streamlit as st
 import pandas as pd
@@ -7,10 +7,7 @@ st.set_page_config(layout="wide")
 
 with st.sidebar:
     st.write("*Contact Info*")
-    # GitHub
     st.image("https://raw.githubusercontent.com/laurasto/onepager/master/images/accenture.aug.2017-072.jpg", width=200)
-    # Local
-    # st.image("./images/accenture.aug.2017-072.jpg", width=200)
     st.write("Laura Astola")
     st.write("AI/ML Computational Science Associate Manager")
     st.write("📱 +31 622 098 982")
@@ -19,7 +16,7 @@ with st.sidebar:
 tab1, tab2, tab3 = st.tabs(["Work Experience", "Education & Languages", "Certificates"],width="stretch")
 
 with tab1:
-    # stlite cannot read external data, so hardcoded again
+    # stlite cannot read external data, so hardcoded
     wdf = pd.DataFrame(columns=['Start', 'Finish', 'Client/Employer', 'Sector', 'Functional', 'Technical'])
     start = ['2025-09-01','2024-09-01','2019-01-01','2018-06-15','2017-10-15','2017-08-17','2014-01-01','2011-01-01','2010-02-01','2006-02-01']
     finish = ['2026-05-13','2025-09-20','2025-8-30','2018-10-15','2018-05-15','2017-10-15','2016-12-30','2013-12-30','2010-12-30','2010-01-23']
@@ -61,19 +58,14 @@ with tab1:
                                 'Technical': st.column_config.TextColumn(help="Double-click a cell for wrapped text")})
 
 with tab2:
-    # GitHub
     st.image("https://raw.githubusercontent.com/laurasto/onepager/master/images/Eindhoven_University_of_Technology_logo.png", width=150)
-    # Local
-    # st.image('images/Eindhoven_University_of_Technology_logo.png', width=150)
     st.write("PhD Applied Mathematics, 2010")
     st.link_button("Thesis in Google Scholar", 'https://scholar.google.com/citations?view_op=view_citation&hl=en&user=ZJZIls8AAAAJ&cstart=20&pagesize=80&citation_for_view=ZJZIls8AAAAJ:WF5omc3nYNoC')
-    # GitHub
     st.image("https://raw.githubusercontent.com/laurasto/onepager/master/images/Helsinki_University_of_Technology_logo.png", width=150)
-    # Local
-    # st.image('images/Helsinki_University_of_Technology_logo.png', width=150)
     st.write('Helsinki University of Technology')
     st.write('Licenciate\'s Degree in Mathematics, 2009')
     st.link_button("Thesis", 'https://alexandria.tue.nl/openaccess/Metis226803.pdf')
+    st.image("https://raw.githubusercontent.com/laurasto/onepager/master/images/Helsinki_University_logo.jpg", width=150)
     st.write('Master\'s Degree in Mathematics, 2000')
 
 with tab3:
@@ -81,8 +73,6 @@ with tab3:
     with col1:
         # Inject CSS for styling since stlite does not allow reading from folder
         st.markdown("""<style>.custom-link-button {
-            display: inline-block;
-            padding: 10px 20px;
             background-color: aquamarine; 
             color: #191970 !important;
             border-radius: 8px;}</style>
@@ -90,11 +80,26 @@ with tab3:
         st.markdown('<a href="https://www.udacity.com/certificate/e/01dd1d86-79b2-11f0-b061-2b2f4f747398" target="_blank" '
             'class="custom-link-button">GitHub Actions</a>',
             unsafe_allow_html=True)
+        st.markdown("""<style>.custom-link-button {
+            background-color: ; 
+            color: #191970 !important;
+            border-radius: 8px;}</style>
+        """, unsafe_allow_html=True)
+        st.markdown('<a href="https://www.udacity.com/certificate/e/bab5e9a8-6eab-11f0-bb2a-5ffcb77ac1ac" target="_blank" '
+            'class="custom-link-button">Fast API</a>',
+            unsafe_allow_html=True)
+        st.markdown("""<style>.custom-link-button {
+                    background-color: ; 
+                    color: #191970 !important;
+                    border-radius: 8px;}</style>
+                """, unsafe_allow_html=True)
+        st.markdown(
+            '<a href="https://www.coursera.org/account/accomplishments/verify/MXJBBXUXWSS5" target="_blank" '
+            'class="custom-link-button">Developing AI Applications on Azure</a>',
+            unsafe_allow_html=True)
 
     with col2:
         st.markdown("""<style>.custom-link-button {
-            display: inline-block;
-            padding: 10px 20px;
             background-color: #FFE4E1; 
             color: #191970 !important;
             border-radius: 8px;}</style>
@@ -102,17 +107,32 @@ with tab3:
         st.markdown('<a href="https://www.coursera.org/account/accomplishments/verify/D6NXQUMG5XZ9" target="_blank" '
             'class="custom-link-button">Apache Spark (TM) SQL for Data Analysts</a>',
             unsafe_allow_html=True)
+        st.markdown("""<style>.custom-link-button {
+                    background-color: ; 
+                    color: #191970 !important;
+                    border-radius: 8px;}</style>
+                """, unsafe_allow_html=True)
+        st.markdown('<a href="https://www.coursera.org/account/accomplishments/specialization/7SYBZGUEQCX9" target="_blank" '
+                    'class="custom-link-button">Deep Learning Specialization</a>',
+                    unsafe_allow_html=True)
 
     with col3:
         st.markdown("""<style>.custom-link-button {
-            display: inline-block;
-            padding: 10px 20px;
             background-color: #E0FFFF; 
             color: #191970 !important;
             border-radius: 8px;}</style>
         """, unsafe_allow_html=True)
         st.markdown('<a href="https://www.coursera.org/account/accomplishments/verify/GL2Y2XL73XLR?utm_source=link&utm_medium=certificate&utm_content=cert_image&utm_campaign=sharing_cta&utm_product=project" target="_blank" '
             'class="custom-link-button">Machine Learning with PySpark: Data Analysis using SQL</a>',
+            unsafe_allow_html=True)
+        st.markdown("""<style>.custom-link-button {
+                    background-color: ; 
+                    color: #191970 !important;
+                    border-radius: 8px;}</style>
+                """, unsafe_allow_html=True)
+        st.markdown(
+            '<a href="https://www.coursera.org/account/accomplishments/verify/EBZMCLUA6A2N?utm_source=link&utm_medium=certificate&utm_content=cert_image&utm_campaign=sharing_cta&utm_product=course" target="_blank" '
+            'class="custom-link-button">Generative AI and Large Language Models</a>',
             unsafe_allow_html=True)
 
 
